6a74b93a494ba5089e689191c9211c7d298c4762
Alexander Bigga <alexander.bigga@slub-dresden.de>
2021-11-24 17:29:54 +0100

    Backup current state

--- kitodo-import.sh
+++ kitodo-import.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 LOCATIONS=$(cat documents.txt)
-KITODO_SOLR=1
+KITODO_SOLR=dlfCore0
 KITODO_PID=2
-EXEC="ddev exec vendor/bin/typo3"
+EXEC="ddev typo3"
 #EXEC="./vendor/bin/typo3"
 
 for LOC in $LOCATIONS; do
